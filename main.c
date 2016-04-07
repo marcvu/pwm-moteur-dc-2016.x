53130444581ed7316fef8975711c4aa57cde359b
jean-michel-gonet <jmgonet@yahoo.com>
2016-04-07 19:29:19 +0200

    Version initiale

--- main.c
+++ main.c
@@ -1,65 +1,159 @@
 #include <xc.h>
-#include <stdio.h>
+#include "test.h"
 
 /**
  * Bits de configuration:
  */
-#pragma config FOSC = INTIO67  // Osc. interne, A6 et A7 comme IO.
-#pragma config IESO = OFF      // Pas d'osc. au démarrage.
-#pragma config FCMEN = OFF     // Pas de monitorage de l'oscillateur.
+#pragma config FOSC = INTIO67   // Osc. interne, A6 et A7 comme IO.
+#pragma config IESO = OFF       // Pas d'osc. au démarrage.
+#pragma config FCMEN = OFF      // Pas de monitorage de l'oscillateur.
 
 // Nécessaires pour ICSP / ICD:
-#pragma config MCLRE = EXTMCLR // RE3 est actif comme master reset.
-#pragma config WDTEN = OFF     // Watchdog inactif.
-#pragma config LVP = OFF       // Single Supply Enable bits off.
+#pragma config MCLRE = EXTMCLR  // RE3 est actif comme master reset.
+#pragma config WDTEN = OFF      // Watchdog inactif.
+#pragma config LVP = OFF        // Single Supply Enable bits off.
+
+typedef enum {
+    AVANT = 0b01,
+    ARRIERE = 0b10
+} Direction;
 
 /**
- * Fonction qui transmet un caractère à la EUSART.
- * Il s'agit de l'implémentation d'une fonction système qui est
- * appelée par <code>printf</code>.
- * Cette implémentation envoie le caractère à la UART. Si un terminal
- * est connecté aux sorties RX / TX, il affichera du texte.
- * @param data Le code ASCII du caractère à afficher.
-*/
-void putch(char data) {
-    while( ! TX1IF);
-    TXREG1 = data;
-}
-
-/**
- * Initialise la sortie 1 de la EUSART.
- * Si le UP est à 1MHz, le Virtual Terminal dans Proteus
- * doit être configuré comme suit:
- * - 1200 bauds.
- * - Transmission 8 bits.
- * - Bit de stop activé.
+ * Indique la direction correspondante à la valeur du potentiomètre.
+ * @param v Valeur du potentiomètre.
+ * @return AVANT ou ARRIERE.
  */
-void initialiseEUSART() {
-    // Pour une fréquence de 1MHz, ceci donne 1200 bauds:
-    SPBRG = 12;
-    SPBRGH = 0;
-    // Configure RC6 et RC7 comme entrées digitales, pour que
-    // la EUSART puisse en prendre le contrôle:
-    TRISCbits.RC6 = 1;
-    TRISCbits.RC7 = 1;
-    
-    // Configure la EUSART:
-    // (BRGH et BRG16 sont à leur valeurs par défaut)
-    // (TX9 est à sa valeur par défaut)
-    RCSTAbits.SPEN = 1;  // Active la EUSART.
-    TXSTAbits.SYNC = 0;  // Mode asynchrone.
-    TXSTAbits.TXEN = 1;  // Active l'émetteur.
-}
-
-/**
- * Point d'entrée du programme.
- */
-void main(void) {
-    unsigned char n;
-    
-    initialiseEUSART();
-
-    for (n = 0; n < 100; n++) {
-        printf("v1 Hello world: %d\r\n", n);
+Direction conversionDirection(unsigned char v) {
+    if (v < 128) {
+        return ARRIERE;
+    } else {
+        return AVANT;
     }
 }
+
+/**
+ * Indique le cycle de travail PWM correspondant à la valeur du potentiomètre.
+ * @param v Valeur du potentiomètre.
+ * @return Cycle de travail du PWM.
+ */
+unsigned char conversionMagnitude(unsigned char v) {
+    unsigned char c;
+    if (v <= 127) {
+        c = 127 - v;
+    } else {
+        c = v - 128;
+    }
+    c <<= 1;
+    return c;
+}
+
+#ifndef TEST
+
+/**
+ * Initialise le hardware.
+ */
+static void hardwareInitialise() {
+    
+    // Prépare Temporisateur 2 pour PWM (compte jusqu'à 255 en 1ms):
+    T2CONbits.T2CKPS = 0;       // Pas de diviseur de fréquence
+    T2CONbits.T2OUTPS = 0b1001; // Diviseur de fréquence de sortie à 1:10
+    T2CONbits.T2OUTPS = 0;      // Pas de diviseur de fréquence à la sortie.
+    T2CONbits.TMR2ON = 1;       // Active le temporisateur.
+    
+    PIE1bits.TMR2IE = 1;        // Active les interruptions ...
+    IPR1bits.TMR2IP = 0;        // ... de basse priorité ...
+    PIR1bits.TMR2IF = 0;        // ... pour le temporisateur 2.
+
+    // Configure PWM 1 pour émettre un signal de 1KHz:
+    ANSELCbits.ANSC2 = 0;
+    TRISCbits.RC2 = 0;
+    
+    CCP1CONbits.P1M = 0;        // Simple PWM sur la sortie P1A
+    CCP1CONbits.CCP1M = 12;     // Active le CCP1 comme PWM.
+    CCPTMRS0bits.C1TSEL = 0;    // Branche le CCP1 sur le temporisateur 2.
+
+    PR2 = 255;                  // Largeur de pulsation: 1ms.
+
+    // Configure RC0 et RC1 pour gérer la direction du moteur:
+    TRISCbits.RC0 = 0;
+    TRISCbits.RC1 = 0;
+    
+    // Active le module de conversion A/D:
+    TRISBbits.RB3 = 1;      // Active RB4 comme entrée.
+    ANSELBbits.ANSB3 = 1;   // Active AN11 comme entrée analogique.
+    ADCON0bits.ADON = 1;    // Allume le module A/D.
+    ADCON0bits.CHS = 9;     // Branche le convertisseur sur AN09
+    ADCON2bits.ADFM = 0;    // Les 8 bits plus signifiants sur ADRESH.
+    ADCON2bits.ACQT = 3;    // Temps d'acquisition à 6 TAD.
+    ADCON2bits.ADCS = 0;    // À 1MHz, le TAD est à 2us.
+
+    PIE1bits.ADIE = 1;      // Active les interruptions A/D
+    IPR1bits.ADIP = 0;      // Interruptions A/D sont de basse priorité.
+
+    // Active les interruptions générales:
+    RCONbits.IPEN = 1;
+    INTCONbits.GIEH = 1;
+    INTCONbits.GIEL = 1;
+}
+
+/**
+ * Point d'entrée des interruptions.
+ */
+void low_priority interrupt interruptionsBassePriorite() {
+    if (PIR1bits.TMR2IF) {
+        PIR1bits.TMR2IF = 0;
+        ADCON0bits.GO = 1;
+    }
+    
+    if (PIR1bits.ADIF) {
+        PIR1bits.ADIF = 0;
+        PORTC = conversionDirection(ADRESH);
+        CCPR1L = conversionMagnitude(ADRESH);
+    }
+}
+
+/**
+ * Point d'entrée pour l'émetteur de radio contrôle.
+ */
+void main(void) {
+    hardwareInitialise();
+
+    while(1);
+}
+#endif
+
+#ifdef TEST
+void testConversionMagnitude() {
+    testeEgaliteEntiers("CM01", conversionMagnitude(0),   254);
+    testeEgaliteEntiers("CM02", conversionMagnitude(1),   252);
+    testeEgaliteEntiers("CM03", conversionMagnitude(2),   250);
+    
+    testeEgaliteEntiers("CM04", conversionMagnitude(125),   4);
+    testeEgaliteEntiers("CM05", conversionMagnitude(126),   2);
+    
+    testeEgaliteEntiers("CM06", conversionMagnitude(127),   0);
+    testeEgaliteEntiers("CM07", conversionMagnitude(128),   0);
+
+    testeEgaliteEntiers("CM08", conversionMagnitude(129),   2);
+    testeEgaliteEntiers("CM09", conversionMagnitude(130),   4);
+    
+    testeEgaliteEntiers("CM10", conversionMagnitude(253), 250);
+    testeEgaliteEntiers("CM11", conversionMagnitude(254), 252);
+    testeEgaliteEntiers("CM12", conversionMagnitude(255), 254);
+}
+void testConversionDirection() {
+    testeEgaliteEntiers("CD01", conversionDirection(  0), ARRIERE);    
+    testeEgaliteEntiers("CD02", conversionDirection(  1), ARRIERE);    
+    testeEgaliteEntiers("CD03", conversionDirection(127), ARRIERE);    
+    testeEgaliteEntiers("CD04", conversionDirection(128), AVANT);
+    testeEgaliteEntiers("CD05", conversionDirection(129), AVANT);
+    testeEgaliteEntiers("CD06", conversionDirection(255), AVANT);    
+}
+void main() {
+    initialiseTests();
+    testConversionMagnitude();
+    testConversionDirection();
+    finaliseTests();
+    while(1);
+}
+#endif
